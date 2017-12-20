3fc2a136c6e53776d8a88c5f50fc4eb85aed5797
samuthojo <samuthojo@gmail.com>
2017-12-20 16:35:20 +0300

    Refactored

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use App;
 
 class LoginController extends Controller
 {
@@ -35,12 +36,60 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        $this->middleware('guest')->except('logout', 'cmsLogout',
+                                           'showCmsLoginForm', 'cmsLogin');
+        $this->middleware('admin')->only('showCmsLoginForm');
     }
 
     public function showLoginForm()
     {
         return view('login');
+    }
+
+    public function showCmsLoginForm()
+    {
+        return view('cms.login');
+    }
+
+    public function cmsLogin(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptCmsLogin($request)) {
+            return $this->sendCmsLoginResponse($request);
+        }
+
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+
+    protected function attemptCmsLogin(Request $request)
+    {
+        $user = App\User::where($request->only('phone_number'))->first();
+        if($user->is_admin) {
+          return $this->guard()->attempt(
+              $this->credentials($request)
+          );
+        }
+        return false;
+    }
+
+    protected function sendCmsLoginResponse(Request $request)
+    {
+        $request->session()->regenerate();
+
+        $this->clearLoginAttempts($request);
+
+        return $this->authenticated($request, $this->guard()->user())
+                ?: redirect()->intended('/admin');
     }
 
     public function username()
@@ -55,5 +104,14 @@ class LoginController extends Controller
         $request->session()->invalidate();
 
         return redirect('/login');
+    }
+
+    public function cmsLogout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        return redirect()->route('cms_login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,22 @@ Route::get('/register', 'KidsCity@register');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin/login', 'Auth\LoginController@showCmsLoginForm')->name('cms_login');
+Route::post('/admin/login', 'Auth\LoginController@cmsLogin')->name('cms_authenticate');
+Route::post('/admin/logout', 'Auth\LoginController@cmsLogout')->name('cms_logout');
+
+Route::get('/admin', 'Brands@index')->name('main');
+
+Route::middleware('auth')->prefix('/admin')->group(function() {
+  // Route::get('/', 'Brands@index')->name('main');
+  Route::get('/brands', 'Brands@index')->name('brands.index');
+  Route::post('/brands', 'Brands@store')->name('brands.store');
+  Route::post('/brands/{brand}', 'Brands@update')->name('brands.update');
+  Route::delete('/brands/{brand}', 'Brands@destroy')->name('brands.destroy');
+
+  Route::get('/categories', 'Categories@index')->name('categories.index');
+  Route::get('/price_categories', 'PriceCategories@index')->name('price_categories.index');
+  Route::get('/products', 'Products@index')->name('products.index');
+  Route::view('/change_password', 'change_password')->name('change_password');
+});
