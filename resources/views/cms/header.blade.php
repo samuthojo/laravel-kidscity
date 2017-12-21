0a83f7647691fd81828f56dfe526f13177d9ed73
samuthojo <samuthojo@gmail.com>
2017-12-21 13:00:12 +0300

    modified:   app/Http/Controllers/PriceCategories.php
    new file:   resources/views/cms/header.blade.php
    
    modified:   resources/views/cms/layouts/cms.blade.php
    
    new file:   resources/views/cms/price_categories.blade.php

---
+++ resources/views/cms/header.blade.php
@@ -0,0 +1,31 @@
+<!-- DataTable Css For Copy, Csv, Excel, Pdf, Print buttons-->
+<link rel="stylesheet" type="text/css"
+  href="https://cdn.datatables.net/buttons/1.4.2/css/buttons.dataTables.min.css">
+
+<!-- DataTable Copy, Csv, Excel, Pdf, Print functionality-->
+<script type="text/javascript" charset="utf8"
+  src="https://cdn.datatables.net/buttons/1.4.2/js/dataTables.buttons.min.js"></script>
+
+<!-- DataTable Copy, Csv, Excel, Pdf, Print functionality-->
+<script type="text/javascript" charset="utf8"
+  src="//cdn.datatables.net/buttons/1.4.2/js/buttons.flash.min.js"></script>
+
+<!-- DataTable Copy, Csv, Excel, Pdf, Print functionality-->
+<script type="text/javascript" charset="utf8"
+  src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+
+<!-- DataTable Copy, Csv, Excel, Pdf, Print functionality-->
+<script type="text/javascript" charset="utf8"
+  src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+
+<!-- DataTable Copy, Csv, Excel, Pdf, Print functionality-->
+<script type="text/javascript" charset="utf8"
+  src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+
+<!-- DataTable Copy, Csv, Excel, Pdf, Print functionality-->
+<script type="text/javascript" charset="utf8"
+  src="https://cdn.datatables.net/buttons/1.4.2/js/buttons.html5.min.js"></script>
+
+<!-- DataTable Copy, Csv, Excel, Pdf, Print functionality-->
+<script type="text/javascript" charset="utf8"
+  src="https://cdn.datatables.net/buttons/1.4.2/js/buttons.print.min.js"></script>
