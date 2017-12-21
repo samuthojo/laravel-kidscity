5ec7042f4eb741b2a5a7190c7d0949ea640934bd
samuthojo <samuthojo@gmail.com>
2017-12-21 12:18:47 +0300

    Successfully created a create_new_brand form

--- resources/views/cms/brands.blade.php
+++ resources/views/cms/brands.blade.php
@@ -1,1 +1,30 @@
 @extends('cms.layouts.cms')
+
+@section('more')
+  <link href="{{asset('css/cms_brands.css')}}" rel="stylesheet">
+@endsection
+
+@section('content')
+  <div class="col-sm-3 brands-form">
+    <h3 class="brand-form-title text-primary">Create New Brand</h3>
+    <form name="brands-form" id="brands-form">
+      <div class="form-group">
+        <input type="text" name="name" placeholder="Brand Name"
+          class="form-control" autofocus>
+      </div>
+      <div class="form-group">
+        <textarea name="description" rows="8" cols="13"
+          class="form-control" placeholder="Short Description"></textarea>
+      </div>
+      <div class="form-group">
+        <input type="file" name="image_url">
+      </div>
+      <div class="form-group">
+        <button type="button" name="create-brand"
+          class="btn btn-primary pull-right">
+          Create
+        </button>
+      </div>
+    </form>
+  </div>
+@endsection
