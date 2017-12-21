45aedf7ba2c54ae3f132dd66c40499fb60d0f3ee
samuthojo <samuthojo@gmail.com>
2017-12-21 11:55:41 +0300

    new file:   public/css/cms_brands.css
    modified:   public/css/cms_styles.css
    
    modified:   resources/views/cms/brands.blade.php
    
    modified:   resources/views/cms/layouts/cms.blade.php

--- resources/views/cms/brands.blade.php
+++ resources/views/cms/brands.blade.php
@@ -1,1 +1,51 @@
 @extends('cms.layouts.cms')
+
+@section('more')
+  <link rel="stylesheet" href="{{asset('css/cms_brands.css')}}">
+@endsection
+
+@section('content')
+<div class="brands-header">
+    <h3 class="title">Brands</h3>
+    <div class="clearfix"></div>
+  </div>
+
+  <div class="brands col-sm-9">
+
+    <div class="row">
+      @foreach($brands as $brand)
+      <div class="col-sm-4">
+        <div class="brand">
+          <div class="brand-picture ">
+            <img src="{{asset('images/brands/' . $brand->image_url)}}"
+              class="img-rounded">
+          </div>
+          <div class="brand-info ">
+            <div class="brand-name ">
+              {{$brand->name}}
+            </div>
+            <div class="brand-description ">
+              {{$brand->description}}
+            </div>
+            <div class="controls">
+              <div class="btn-group pull-right">
+                <button type="button" name="button" class="btn btn-warning"
+                  title="edit">
+                  <span class="glyphicon glyphicon-pencil"></span>
+                </button>
+                <button type="button" name="button" class="btn btn-danger"
+                  title="delete">
+                  <span class="glyphicon glyphicon-trash"></span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      @endforeach
+   </div>
+  </div>
+
+  </div>
+
+@endsection
--- resources/views/cms/layouts/cms.blade.php
+++ resources/views/cms/layouts/cms.blade.php
@@ -16,6 +16,8 @@
 
     <link rel="stylesheet" href="{{asset('css/cms_styles.css')}}">
 
+    @yield('more')
+
     <script
     src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
     </script>
