23104a5d11c5f5f9fdf403473cb9b7e669014f83
samuthojo <samuthojo@gmail.com>
2017-12-21 12:38:41 +0300

    Added the index method in Categories Controller

--- app/Http/Controllers/Categories.php
+++ app/Http/Controllers/Categories.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App;
 
 class Categories extends Controller
 {
-    //
+    public function index()
+    {
+      $categories = App\Category::all();
+      return view('cms.categories', compact('categories'));
+    }
 }
