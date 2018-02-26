c102cc005f663923b9689cad0a6a0705a843fdda
Walter Kimaro <wakyj07@gmail.com>
2018-02-26 17:08:53 +0300

    Fix back arro error

--- resources/views/mobile/tpl/search_area.blade.php
+++ resources/views/mobile/tpl/search_area.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-bg" onclick="stopSearching()"></div>
     <div id="content">
         <form id="mobSearchBar" class="an-action-bar layout center justified">
-            <button type="button" id="backBtn" class="layout center action-button" onclick="stopSearching()">
+            <button type="button" id="backBtn" class="layout center action-button" onclick="function back(){window.history.back();};back()">
                 <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 0h24v24H0z" fill="none"/>
                     <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
